11520d5cc5856bb6cc04a7439a3fd67a1e7c4831
snahian <contactsajednahian@gmail.com>
2019-06-14 06:41:14 -0400

    init commit

---
+++ Assets/Manager.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+    public string[] fieldDirections = new string[2] { "out", "in" };
+    public string direction;
+    public int particleSign;
+    public string fieldDirection;
+    public GameObject field, particle;
+    public Sprite outfield, infield, electron, proton;
+    public string answer;
+    Particle particleController;
+	// Use this for initialization
+	void Start () {
+        direction = "down";
+        fieldDirection = fieldDirections[Random.Range(0, 2)];
+        if (Random.Range(0, 2) == 0)
+        {
+            particleSign = 1;
+        }  else
+        {
+            particleSign = -1;
+        }
+        setupGame();
+        particleController = particle.GetComponent<Particle>();
+        particleController.goDown();
+        answer = calculateCorrectAnswer(direction, fieldDirection, particleSign);
+        print("Answer: " + answer);
+    }
+
+    void setupGame() {
+        if (fieldDirection == "out")
+        {
+            field.GetComponent<SpriteRenderer>().sprite = outfield;
+        } else
+        {
+            field.GetComponent<SpriteRenderer>().sprite = infield;
+        }
+
+        if (particleSign > 0)
+        {
+            particle.GetComponent<SpriteRenderer>().sprite = proton;
+        }
+        else
+        {
+            particle.GetComponent<SpriteRenderer>().sprite = electron;
+        }
+    }
+
+    string calculateCorrectAnswer(string direction, string field, int charge)
+    {
+        string answer = "";
+        if (charge > 0)
+        {
+            if (field == "in")
+            {
+                switch (direction)
+                {
+                    case "down":
+                        answer = "right";
+                        break;
+                    case "up":
+                        answer = "left";
+                        break;
+                    case "left":
+                        answer = "down";
+                        break;
+                    default:
+                        answer = "up";
+                        break;
+                }
+            }
+            else
+            {
+                switch (direction)
+                {
+                    case "down":
+                        answer = "left";
+                        break;
+                    case "up":
+                        answer = "right";
+                        break;
+                    case "left":
+                        answer = "up";
+                        break;
+                    default:
+                        answer = "down";
+                        break;
+                }
+            }
+        }
+        else
+        {
+            if (field == "in")
+            {
+                switch (direction)
+                {
+                    case "down":
+                        answer = "left";
+                        break;
+                    case "up":
+                        answer = "right";
+                        break;
+                    case "left":
+                        answer = "up";
+                        break;
+                    default:
+                        answer = "down";
+                        break;
+                }
+            }
+            else
+            {
+                switch (direction)
+                {
+                    case "down":
+                        answer = "right";
+                        break;
+                    case "up":
+                        answer = "left";
+                        break;
+                    case "left":
+                        answer = "down";
+                        break;
+                    default:
+                        answer = "up";
+                        break;
+                }
+            }
+        }
+        return answer;
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LeftButton ()
+    {
+        if (answer == "left")
+        {
+            particleController.curveLeft();
+        }
+    }
+
+    public void RightButton()
+    {
+        if (answer == "right")
+        {
+            particleController.curveRight();
+        }
+    }
+
+    public void UpButton()
+    {
+        if (answer == "up")
+        {
+            particleController.curveUp();
+        }
+    }
+
+    public void DownButton()
+    {
+        if (answer == "down")
+        {
+            particleController.curveDown();
+        }
+    }
+
+    public void NewField (string direction)
+    {
+        fieldDirection = fieldDirections[Random.Range(0, 2)];
+        if (fieldDirection == "out")
+        {
+            field.GetComponent<SpriteRenderer>().sprite = outfield;
+        }
+        else
+        {
+            field.GetComponent<SpriteRenderer>().sprite = infield;
+        }
+        answer = calculateCorrectAnswer(direction, fieldDirection, particleSign);
+        print("Answer: " + answer);
+
+        if (direction == "left")
+        {
+            particleController.goLeft();
+        }
+        if (direction == "right")
+        {
+            particleController.goRight();
+        }
+        if (direction == "up")
+        {
+            particleController.goUp();
+        }
+        if (direction == "down")
+        {
+            particleController.goDown();
+        }
+    }
+}
---
+++ Assets/Particle.cs
@@ -0,0 +1,246 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour {
+    public Transform topLeft, topRight, botRight, botLeft;
+    public Transform resetLeft, resetRight, resetTop, resetBot, middle;
+    public string direction = "";
+    private float speed = 6f;
+    public float curveSpeed = 5.4f;
+    Manager manager;
+	// Use this for initialization
+	void Start () {
+        manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<Manager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (direction == "down")
+        {
+            if (transform.position.y >= topLeft.position.y)
+            {
+                transform.position += new Vector3(0, -1, 0) * speed * Time.deltaTime;
+            } else
+            {
+                direction = "";
+            }
+        }
+        if (direction == "left")
+        {
+            if (transform.position.x >= topRight.position.x)
+            {
+                transform.position += new Vector3(-1, 0, 0) * speed * Time.deltaTime;
+            }
+            else
+            {
+                direction = "";
+            }
+        }
+        if (direction == "right")
+        {
+            if (transform.position.x <= topLeft.position.x)
+            {
+                transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+            }
+            else
+            {
+                direction = "";
+            }
+        }
+        if (direction == "up")
+        {
+            if (transform.position.y <= botLeft.position.y)
+            {
+                transform.position += new Vector3(0, 1, 0) * speed * Time.deltaTime;
+            }
+            else
+            {
+                direction = "";
+            }
+        }
+        if (direction == "curveLeft")
+        {
+            if (transform.position.x <= topLeft.transform.position.x)
+            {
+                direction = "resetLeft";
+            } else
+            {
+                if (transform.position.y > middle.position.y)
+                {
+                    transform.RotateAround(topLeft.position, new Vector3(0, 0, -1), curveSpeed);
+                } else
+                {
+                    transform.RotateAround(botLeft.position, new Vector3(0, 0, 1), curveSpeed);
+                }
+                transform.eulerAngles = new Vector3(
+                    transform.eulerAngles.x,
+                    transform.eulerAngles.y,
+                    0
+                );
+            }
+        }
+        if (direction == "curveRight")
+        {
+            if (transform.position.x >= topRight.transform.position.x)
+            {
+                direction = "resetRight";
+            }
+            else
+            {
+                if (transform.position.y > middle.position.y)
+                {
+                    transform.RotateAround(topRight.position, new Vector3(0, 0, 1), curveSpeed);
+                } else
+                {
+                    transform.RotateAround(botRight.position, new Vector3(0, 0, -1), curveSpeed);
+                }
+                transform.eulerAngles = new Vector3(
+                    transform.eulerAngles.x,
+                    transform.eulerAngles.y,
+                    0
+                );
+            }
+        }
+        if (direction == "curveDown")
+        {
+            if (transform.position.y <= botLeft.transform.position.y)
+            {
+                direction = "resetDown";
+            }
+            else
+            {
+                if (transform.position.x > middle.position.x)
+                {
+                    transform.RotateAround(botRight.position, new Vector3(0, 0, 1), curveSpeed);
+                } else
+                {
+                    transform.RotateAround(botLeft.position, new Vector3(0, 0, -1), curveSpeed);
+                }
+                transform.eulerAngles = new Vector3(
+                    transform.eulerAngles.x,
+                    transform.eulerAngles.y,
+                    0
+                );
+            }
+        }
+
+        if (direction == "curveUp")
+        {
+            if (transform.position.y >= topLeft.transform.position.y)
+            {
+                direction = "resetUp";
+            }
+            else
+            {
+                if (transform.position.x > middle.position.x)
+                {
+                    transform.RotateAround(topRight.position, new Vector3(0, 0, -1), curveSpeed);
+                }
+                else
+                {
+                    transform.RotateAround(topLeft.position, new Vector3(0, 0, 1), curveSpeed);
+                }
+                transform.eulerAngles = new Vector3(
+                    transform.eulerAngles.x,
+                    transform.eulerAngles.y,
+                    0
+                );
+            }
+        }
+
+
+        if (direction == "resetLeft")
+        {
+            if (transform.position.x >= resetLeft.position.x)
+            {
+                transform.position += new Vector3(-1, 0, 0) * speed * Time.deltaTime;
+            }
+            else
+            {
+                direction = "";
+                transform.position = resetRight.position;
+                manager.NewField("left");
+            }
+        }
+        if (direction == "resetUp")
+        {
+            if (transform.position.y <= resetTop.position.y)
+            {
+                transform.position += new Vector3(0, 1, 0) * speed * Time.deltaTime;
+            }
+            else
+            {
+                direction = "";
+                transform.position = resetBot.position;
+                manager.NewField("up");
+            }
+        }
+        if (direction == "resetRight")
+        {
+            if (transform.position.x <= resetRight.position.x)
+            {
+                transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+            }
+            else
+            {
+                direction = "";
+                transform.position = resetLeft.position;
+                manager.NewField("right");
+            }
+        }
+        if (direction == "resetDown")
+        {
+            if (transform.position.y >= resetBot.position.x)
+            {
+                transform.position += new Vector3(0, -1, 0) * speed * Time.deltaTime;
+            }
+            else
+            {
+                direction = "";
+                transform.position = resetTop.position;
+                manager.NewField("down");
+            }
+        }
+    }
+
+    public void goDown ()
+    {
+        direction = "down";
+    }
+
+    public void goUp()
+    {
+        direction = "up";
+    }
+
+    public void goLeft()
+    {
+        direction = "left";
+    }
+
+    public void goRight()
+    {
+        direction = "right";
+    }
+
+    public void curveLeft ()
+    {
+        direction = "curveLeft";
+    }
+
+    public void curveRight ()
+    {
+        direction = "curveRight";
+    }
+
+    public void curveUp ()
+    {
+        direction = "curveUp";
+    }
+
+    public void curveDown()
+    {
+        direction = "curveDown";
+    }
+}
